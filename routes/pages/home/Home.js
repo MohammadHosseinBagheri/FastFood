69f96cfe6610909e51361f554ca820b4a2f900bd
MohammadHosseinBagheri <mohammadbagheri228@gmail.com>
2019-11-06 02:43:03 +0330

    add best restaurant ui

--- routes/pages/home/Home.js
+++ routes/pages/home/Home.js
@@ -1,18 +1,41 @@
 import React, {Component} from 'react';
-import {View, Text, StatusBar} from 'react-native';
+import {View, Text, StatusBar, Image, TouchableOpacity} from 'react-native';
 import items from './Items';
 import {FlatList} from 'react-native-gesture-handler';
 import MyHeader from '../../../components/Header/MyHeader';
-import {Icon} from 'native-base';
+import {Icon, Card, CardItem, Content, Button, Left, Right} from 'native-base';
 import MyModal from '../../../components/Modal/MyModal';
 
 class Basicitem extends Component {
   render() {
     const {item} = this.props;
     return (
-      <View style={{justifyContent:'center',alignItems:'center'}}>
-        <Text>{item.txt}</Text>
-        <Text>{item.body}</Text>
+      <View style={{margin: 5}}>
+        <TouchableOpacity onPress={() => console.log('ok')} activeOpacity={0.9}>
+          <View>
+            <Card
+              style={{
+                width: 200,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <CardItem style={{elevation: 10}}>
+                <Content>
+                  <View
+                    style={{justifyContent: 'center', alignItems: 'center'}}>
+                    <Text style={{textAlign: 'center'}}>{item.name}</Text>
+                    <Image
+                      style={{width: 100, height: 100}}
+                      resizeMode={'stretch'}
+                      source={require('../../../assets/img/food4.png')}
+                    />
+                    <Text numberOfLines={3}>{item.description}</Text>
+                  </View>
+                </Content>
+              </CardItem>
+            </Card>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -21,17 +44,28 @@ class Basicitem extends Component {
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      restaurants: [],
+    };
   }
   componentDidMount() {
-    console.log(items[0].txt);
+    this.getDataFromApi();
   }
   openMenu() {
     this.refs.myModal.modalOpen();
   }
+  async getDataFromApi() {
+    const response = await fetch('http://10.0.2.2:3000/api/restaurants');
+    const responseJsaon = await response.json();
+    console.log(responseJsaon);
+    await this.setState({
+      restaurants: responseJsaon,
+    });
+    console.log(this.state.restaurants);
+  }
   render() {
     return (
-      <View style={{flex:1,backgroundColor:'#E0E0E0'}}>
+      <View style={{flex: 1, backgroundColor: '#E0E0E0'}}>
         <MyHeader
           right={
             <Icon
@@ -42,12 +76,40 @@ class Home extends Component {
           }
           left={<Icon name={'mail'} style={{color: 'white'}} />}
         />
-        <View style={{flex:0.4}}>
-          <Text>برترین ها</Text>
+        <View style={{flex: 0.5}}>
+          <View
+            style={{
+              margin: 5,
+              marginBottom: 0,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}>
+            <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
+              <Icon name={'arrow-dropleft'} style={{color:'green'}}/>
+              <Button transparent style={{marginLeft:2}}>
+                <Text
+                  style={{
+                    color: 'green',
+                    fontFamily: 'IRANSansMobile_Light',
+                    fontSize: 14,
+                  }}>
+                  بیشتر
+                </Text>
+              </Button>
+            </View>
+            <Text
+              style={{
+                fontSize: 16,
+                fontFamily: 'IRANSansMobile_Light',
+              }}>
+              برترین ها
+            </Text>
+          </View>
           <FlatList
             horizontal={true}
-            data={items}
-            keyExtractor={index => index}
+            data={this.state.restaurants}
+            keyExtractor={index => index.toString()}
             renderItem={({item}) => {
               return <Basicitem item={item} />;
             }}
@@ -55,6 +117,7 @@ class Home extends Component {
         </View>
         <View style={{flex: 0.5}}>
           <Text>دسته بندی ها</Text>
+          </Image>
         </View>
         <MyModal ref={'myModal'} />
       </View>
