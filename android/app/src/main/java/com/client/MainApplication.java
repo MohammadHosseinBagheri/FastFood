73b36a587e1f7e526aa3ccba39e2e89e7de3134d
MohammadHosseinBagheri <mohammadbagheri228@gmail.com>
2019-12-18 13:05:28 +0330

    add map configuration

--- android/app/src/main/java/com/client/MainApplication.java
+++ android/app/src/main/java/com/client/MainApplication.java
@@ -4,13 +4,14 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.geolocation.GeolocationPackage;
 import com.reactnativecommunity.viewpager.RNCViewPagerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-
+import com.airbnb.android.react.maps.MapsPackage;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
