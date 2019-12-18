5100434a64183b151655ec1dc7f2599b98f383e7
MohammadHosseinBagheri <mohammadbagheri228@gmail.com>
2019-12-19 00:58:00 +0330

    successful delete menu food

--- routes/pages/restaurants/Menus.js
+++ routes/pages/restaurants/Menus.js
@@ -4,10 +4,37 @@ import MyHeader from '../../../components/Header/MyHeader';
 import {Icon, Card, CardItem, Button, Right} from 'native-base';
 import AddNewFood from './Modal/AddNewFood';
 class ShowMenu extends Component {
+  async fetchDataToRemove(item) {
+    const response = await fetch(
+      'http://10.0.2.2:3000/restaurants/menus/remove',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'Post',
+        body: JSON.stringify({
+          data: item,
+        }),
+      },
+    );
+    const responseJson = await response.json();
+    console.log(responseJson);
+    const status = await responseJson.status;
+    if (status != 200) {
+      alert('در حذف ایتم مورد نظر مشکل پیش اومد :(');
+      return;
+    } else {
+      alert('آیتم مورد نظر با موفقیت حذف شد :)');
+    }
+  }
+  componentDidMount() {
+    console.log(this.props);
+  }
   render() {
     const {item} = this.props;
     return (
-      <View style={{flex: 1, margin: 5, borderRadius: 10}}>
+      <View
+        style={{flex: 1, margin: 5, borderRadius: 10, flexDirection: 'column'}}>
         <Card style={{borderRadius: 10}}>
           <CardItem
             style={{
@@ -28,17 +55,38 @@ class ShowMenu extends Component {
               {item.description}
             </Text>
           </CardItem>
+        </Card>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginLeft: 2,
+            marginRight: 2,
+          }}>
           <Button
-            full
+            transparent
             style={{
-              backgroundColor: 'white',
-              borderRadius: 10,
-              borderColor: '#E91E63',
-              borderWidth: 1,
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
             }}>
             <Text style={{fontFamily: 'IRANSansMobile'}}>ویرایش</Text>
+            <Icon name={'create'} style={{color: 'green'}} />
           </Button>
-        </Card>
+          <Button
+            transparent
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            onPress={this.fetchDataToRemove.bind(this, item)}>
+            <Text style={{fontFamily: 'IRANSansMobile'}}>حذف</Text>
+            <Icon name={'remove-circle'} style={{color: 'red'}} />
+          </Button>
+        </View>
       </View>
     );
   }
@@ -49,29 +97,30 @@ class Menus extends Component {
     super(props);
     this.state = {
       data: [],
-      restaurantData:""
+      restaurantData: '',
     };
   }
   addNewFood() {}
   async componentDidMount() {
     //console.log(this.props);
     const dataServer = await this.props.navigation.state.params.Menudata;
-    const restaurantData = await this.props.navigation.state.params.restaurantData;
-   //await console.log(dataServer)
+    const restaurantData = await this.props.navigation.state.params
+      .restaurantData;
+    //await console.log(dataServer)
     await this.setState({
       data: dataServer,
-      restaurantData:restaurantData
+      restaurantData: restaurantData,
     });
     // console.log(this.state.data);
     // console.log(this.props);
-    console.log(this.state.restaurantData)
+    console.log(this.state.restaurantData);
   }
   openNewFood() {
     this.refs.addNewFood.open();
   }
   render() {
     return (
-      <View style={{flex: 1,backgroundColor:'#EEEEEE'}}>
+      <View style={{flex: 1, backgroundColor: '#EEEEEE'}}>
         <MyHeader
           left={
             <Icon
@@ -113,10 +162,14 @@ class Menus extends Component {
         </View>
         <FlatList
           data={this.state.data}
+          extraData={data}
           numColumns={2}
           renderItem={({item}) => <ShowMenu item={item} />}
         />
-        <AddNewFood ref={'addNewFood'} restaurantData={this.state.restaurantData} />
+        <AddNewFood
+          ref={'addNewFood'}
+          restaurantData={this.state.restaurantData}
+        />
       </View>
     );
   }
